1f2f32aee7f7c5d9079b69695f2b5701eebd41b1
Romain Francois <romain.francois@gmail.com>
2020-06-29 16:20:26 +0100

    Adding animations & assets

--- BipTheGuy/ViewController.swift
+++ BipTheGuy/ViewController.swift
@@ -7,14 +7,79 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func playSound(name: String) {
+        if let sound = NSDataAsset(name: name) {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print(" ERROR: \(error.localizedDescription)")
+            }
+        } else {
+            print("ERROR: Could not read data from file \(name)")
+        }
+    }
 
-
+    @IBAction func punchButtonPressed(_ sender: UIButton) {
+        let originalImageFrame = imageView.frame
+        let imageWidthShrink: CGFloat = 20
+        let imageHeightShrink: CGFloat = 20
+        let smallerImageFrame = CGRect(
+            x: imageView.frame.origin.x + imageWidthShrink,
+            y: imageView.frame.origin.y + imageHeightShrink,
+            width: imageView.frame.width - (2 * imageWidthShrink),
+            height: imageView.frame.height - (2 * imageHeightShrink))
+        
+        imageView.frame = smallerImageFrame
+        
+        playSound(name: "punchSound")
+        
+        UIView.animate(
+            withDuration: 0.25,
+            delay: 0.0,
+            usingSpringWithDamping: 0.2,
+            initialSpringVelocity: 10.0,
+            options: [],
+            animations: {
+                self.imageView.frame = originalImageFrame
+        })
+    }
+    
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        let originalImageFrame = imageView.frame
+        let imageWidthShrink: CGFloat = 20
+        let imageHeightShrink: CGFloat = 20
+        let smallerImageFrame = CGRect(
+            x: imageView.frame.origin.x + imageWidthShrink,
+            y: imageView.frame.origin.y + imageHeightShrink,
+            width: imageView.frame.width - (2 * imageWidthShrink),
+            height: imageView.frame.height - (2 * imageHeightShrink))
+        
+        imageView.frame = smallerImageFrame
+        
+        playSound(name: "punchSound")
+        
+        UIView.animate(
+            withDuration: 0.25,
+            delay: 0.0,
+            usingSpringWithDamping: 0.2,
+            initialSpringVelocity: 10.0,
+            options: [],
+            animations: {
+                self.imageView.frame = originalImageFrame
+        })
+    }
 }
-
