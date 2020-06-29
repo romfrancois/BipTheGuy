f389477d3c79cfba23ddea2e2e3c4b07dcf5bf2b
Romain Francois <romain.francois@gmail.com>
2020-06-29 15:39:48 +0100

    Initial Commit

---
+++ BipTheGuy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BipTheGuy
+//
+//  Created by Romain Francois on 29/06/2020.
+//  Copyright © 2020 Romain Francois. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
